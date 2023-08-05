2263b123f696470a3ff06075f6321a371c082b78
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-05 18:59:01 -0500

    Feat: Created Maze

--- src/Components/SoftMazeSquare.jsx
+++ src/Components/SoftMazeSquare.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import { useState } from "react"
 
-export const SoftMazeSquare = () => {
+export const SoftMazeSquare = ({ borders }) => {
   return (
-    <div>SoftMazeSquare</div>
+    <div className={`w-[70px] h-[70px] border-black ${borders} hover:bg-cue-red`}>
+    </div>
   )
 }
--- src/Pages/SoftMaze.jsx
+++ src/Pages/SoftMaze.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import { SoftMazeSquare } from "../Components/SoftMazeSquare"
 
 export const SoftMaze = () => {
   return (
-    <div>
-      <h1>HERE IS THE MAZEEEE</h1>
-    </div>
+    <section className='flex flex-col items-center'>
+      <h1 className='m-10 text-4xl font-cocogoose font-bold'>HERE IS THE MAZE</h1>
+      <div className='w-[700px] h-[700px]'>
+        <div className='grid grid-cols-10 gap-0'>
+          <SoftMazeSquare borders='border-t-8 border-l-8 border-b-4' />
+          <SoftMazeSquare borders='border-t-8' />
+          <SoftMazeSquare borders='border-t-8 border-r-4' />
+          <SoftMazeSquare borders='border-t-8 border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-8 border-r-4' />
+          <SoftMazeSquare borders='border-t-8 border-l-4' />
+          <SoftMazeSquare borders='border-t-8 border-r-4' />
+          <SoftMazeSquare borders='border-t-8 border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-8 border-b-4' />
+          <SoftMazeSquare borders='border-t-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-t-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4' />
+          <SoftMazeSquare borders='border-r-8 border-b-4' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-b-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-t-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-r-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8' />
+          <SoftMazeSquare borders='border-t-4 border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-r-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4' />
+          <SoftMazeSquare borders='border-t-4' />
+          <SoftMazeSquare borders='border-r-8 border-b-4' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-r-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='' />
+          <SoftMazeSquare borders='' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-t-4 border-l-8' />
+          <SoftMazeSquare borders='border-t-4 border-b-4' />
+          <SoftMazeSquare borders='border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4' />
+          <SoftMazeSquare borders='border-t-4' />
+          <SoftMazeSquare borders='border-b-4' />
+          <SoftMazeSquare borders='border-r-8 border-b-4' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-l-8 border-r-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4 border-b-4' />
+          <SoftMazeSquare borders='border-b-4' />
+          <SoftMazeSquare borders='border-r-4' />
+          <SoftMazeSquare borders='border-l-4' />
+          <SoftMazeSquare borders='border-b-4' />
+          <SoftMazeSquare borders='border-r-4' />
+          <SoftMazeSquare borders='border-l-4 border-r-4 border-b-4' />
+          <SoftMazeSquare borders='border-t-4 border-l-4' />
+          <SoftMazeSquare borders='border-t-4 border-r-8' />
+        </div>
+        <div className='grid grid-cols-10'>
+          <SoftMazeSquare borders='border-b-8' />
+          <SoftMazeSquare borders='border-t-4 border-b-8' />
+          <SoftMazeSquare borders='border-t-4 border-b-8' />
+          <SoftMazeSquare borders='border-b-8' />
+          <SoftMazeSquare borders='border-b-8' />
+          <SoftMazeSquare borders='border-t-4 border-r-4 border-b-8' />
+          <SoftMazeSquare borders='border-l-4 border-b-8' />
+          <SoftMazeSquare borders='border-t-4 border-b-8' />
+          <SoftMazeSquare borders='border-r-4 border-b-8' />
+          <SoftMazeSquare borders='border-r-8 border-l-4 border-b-8' />
+        </div>
+      </div>
+    </section>
   )
 }
