746248ff4f4aa1eeb3e4739024c77d17ed84088f
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-13 00:47:27 -0500

    Feat: Finished maze square

--- src/Components/SoftMazeSquare.jsx
+++ src/Components/SoftMazeSquare.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react"
 
-export const SoftMazeSquare = ({ borders }) => {
+export const SoftMazeSquare = ({ borders, coordinate, onSquareClick }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleClick = () => {
+    setIsSelected(!isSelected);
+    onSquareClick(coordinate[0], coordinate[1]);
+  }
+
   return (
-    <div className={`w-[70px] h-[70px] border-black ${borders} hover:bg-cue-red`}>
+    <div
+      onClick={handleClick}
+      className={`w-[70px] h-[70px] flex justify-center items-center border-white ${borders} hover:cursor-pointer`}
+    >
+      <div className={`w-[50px] h-[50px] rounded-full ${isSelected ? 'bg-turquoise animate-jump animate-once' : ''} hover:bg-turquoise`} />
     </div>
   )
 }
