77d34666a316b419de3da227699d57fe6ebf2414
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-14 00:48:17 -0500

    Feat: Finished Maze

--- src/Pages/SoftMaze.jsx
+++ src/Pages/SoftMaze.jsx
@@ -1,70 +1,86 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@nextui-org/react";
 import Swal from "sweetalert2";
 
 import mazeBorders from "../utils/mazeBorders";
+import mazeWelcome from "../utils/mazeWelcome";
 import { SnowBackground } from "../Components/SnowBackground/SnowBackground";
-import { SoftMazeSquare } from "../Components/SoftMazeSquare"
+import { SoftMazeSquare } from "../Components/SoftMazeSquare";
 
-
-const solutionPaths = [
-  ['(9, 0)','(9, 1)','(9, 2)','(9, 3)','(9, 4)','(8, 4)','(8, 5)','(7, 5)','(6, 5)','(5, 5)','(5, 4)','(4, 4)','(4, 5)','(3, 5)','(2, 5)','(1, 5)','(0, 5)','(0, 6)','(1, 6)','(1, 7)','(1, 8)','(1, 9)','(0, 9)'],
-  ['(9, 0)','(8, 0)','(7, 0)','(7, 1)','(7, 2)','(6, 2)','(5, 2)','(5, 1)','(4, 1)','(4, 2)','(4, 3)','(3, 3)','(3, 2)','(2, 2)','(2, 1)','(1, 1)','(0, 1)','(0, 2)','(1, 2)','(1, 3)','(2, 3)','(2, 4)','(1, 4)','(1, 5)','(0, 5)','(0, 6)','(1, 6)','(1, 7)','(1, 8)','(1, 9)','(0, 9)']
-];
-
-const sortedShortestPath = solutionPaths[0].slice().sort().toString();
-const sortedLongestPath = solutionPaths[1].slice().sort().toString();
-
-const reviewToken = () => {
-  const token = 'SANTIAGO';
-  let inputToken = '';
-
-  Swal.fire({
-    title: '!Lo has conseguido! Ve al siguiente reto y consigue el código para continuar acá',
-    input: 'text',
-    showCancelButton: false,
-    confirmButtonText: 'Verificar código',
-    confirmButtonColor: 'rgb(0, 218, 185)',
-    showLoaderOnConfirm: true,
-    preConfirm: (userInput) => inputToken = userInput,
-    allowOutsideClick: false
-  }).then((result) => {
-    if (result.isConfirmed) {
-      if (inputToken !== token) {
-        Swal.fire(
-          'Código equivocado', '', 'error'
-        ).then((result) => {
-          if (result.isConfirmed) reviewToken();
-        })
-        // Link to the nex level
-      } else alert("WIIIIII")
-    }
-  })
-}
-
-const reviewSelectedPath = (selection) => {
-  const sortedSelection = selection.slice().sort().toString();
-
-  if (sortedSelection === sortedLongestPath) {
-    Swal.fire({
-      title: '!Esa es una solución!',
-      html: 'Pero no la más óptima :(<br>Sigue buscando',
-      timer: 4000,
-      timerProgressBar: true,
-    });
-  } else if (sortedSelection === sortedShortestPath) {
-    reviewToken();
-  } else {
-    Swal.fire({
-      title: '¿Esa es una solución?',
-      html: 'Yo no lo creo',
-      timer: 4000,
-      timerProgressBar: true,
-    });
-  }
-}
 
 export const SoftMaze = () => {
+  const solutionPaths = [
+    ['(9, 0)','(9, 1)','(9, 2)','(9, 3)','(9, 4)','(8, 4)','(8, 5)','(7, 5)','(6, 5)','(5, 5)','(5, 4)','(4, 4)','(4, 5)','(3, 5)','(2, 5)','(1, 5)','(0, 5)','(0, 6)','(1, 6)','(1, 7)','(1, 8)','(1, 9)','(0, 9)'],
+    ['(9, 0)','(8, 0)','(7, 0)','(7, 1)','(7, 2)','(6, 2)','(5, 2)','(5, 1)','(4, 1)','(4, 2)','(4, 3)','(3, 3)','(3, 2)','(2, 2)','(2, 1)','(1, 1)','(0, 1)','(0, 2)','(1, 2)','(1, 3)','(2, 3)','(2, 4)','(1, 4)','(1, 5)','(0, 5)','(0, 6)','(1, 6)','(1, 7)','(1, 8)','(1, 9)','(0, 9)']
+  ];
+  
+  const sortedShortestPath = solutionPaths[0].slice().sort().toString();
+  const sortedLongestPath = solutionPaths[1].slice().sort().toString();
+
+  const navigate = useNavigate();
   const selectedSquares = [];
+
+  useEffect(() => {
+    mazeWelcome();
+
+    window.history.pushState(null, null, window.location.href);
+    window.onpopstate = function () {
+      window.history.go(1);
+    };
+
+    window.onbeforeunload = function () {
+      return 0;
+    };
+  })
+
+  const reviewToken = () => {
+    const token = 'SANTIAGO';
+    let inputToken = '';
+  
+    Swal.fire({
+      title: '!Lo has conseguido! Ve al siguiente reto y consigue el código para continuar acá',
+      input: 'text',
+      showCancelButton: false,
+      confirmButtonText: 'Verificar código',
+      confirmButtonColor: 'rgb(0, 218, 185)',
+      showLoaderOnConfirm: true,
+      preConfirm: (userInput) => inputToken = userInput,
+      allowOutsideClick: false
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (inputToken !== token) {
+          Swal.fire(
+            'Código equivocado', '', 'error'
+          ).then((result) => {
+            if (result.isConfirmed) reviewToken();
+          })
+        } else navigate('/soft-cards');
+      }
+    })
+  }
+
+  const reviewSelectedPath = (selection) => {
+    const sortedSelection = selection.slice().sort().toString();
+  
+    if (sortedSelection === sortedLongestPath) {
+      Swal.fire({
+        title: '!Esa es una solución!',
+        html: 'Pero no la más óptima :(<br>Sigue buscando',
+        timer: 4000,
+        timerProgressBar: true,
+      });
+    } else if (sortedSelection === sortedShortestPath) {
+      reviewToken();
+    } else {
+      Swal.fire({
+        title: '¿Esa es una solución?',
+        html: 'Yo no lo creo',
+        timer: 4000,
+        timerProgressBar: true,
+      });
+    }
+  }
 
   const handleSquareClick = (row, col) => {
     const clickedSquare = `(${row}, ${col})`;
