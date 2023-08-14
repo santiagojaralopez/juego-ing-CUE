21a1d26f0a59f42e943da4d983b076b390e74dfa
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-14 00:47:37 -0500

    Feat: Added the maze welcome message

--- src/utils/mazeWelcome.js
+++ src/utils/mazeWelcome.js
@@ -1,7 +1,11 @@
 import Swal from "sweetalert2";
 
 const mazeWelcome = () => {
-  
+  Swal.fire({
+    title: 'Reto del Laberinto',
+    html: 'Al igual que en la ingeniería de software, acá enfrentarás decisiones cruciales.<br><br>El laberinto tiene dos caminos para llegar al final, ¡pero solo uno es el más corto y eficiente!<br><br>Recuerda, en la programación, siempre hay múltiples soluciones, ¡pero tu objetivo es encontrar la mejor!',
+    allowOutsideClick: false
+  })
 }
 
 export default mazeWelcome;
