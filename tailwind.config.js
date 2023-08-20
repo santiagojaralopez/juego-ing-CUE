959d396bc3dcb8d5fb2e925dd8b4feba5225be82
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-20 18:38:17 -0500

    Feat: Added background image for dev team intro section

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,11 @@ module.exports = {
       cocogoose: ["Cocogoose"],
       calibri: ['Calibri', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'dev-team-intro': "linear-gradient(to right, rgba(19, 71, 117, .8), rgba(45, 116, 179, .8)), url('https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+     },
+    },
   },
   plugins: [
     require('tailwindcss-animated'),
