905e085f99eff7530ffc915a510cd1685e4e6d24
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-20 14:48:58 -0500

    Feat: Finished Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom"
+import { Button } from "@nextui-org/react"
+
+
 export const Home = () => {
   return (
-    <>
-      <h1 className="text-cue-red">HOLAAAA</h1>
-    </>
+    <div className="w-screen h-screen flex flex-col gap-20 items-center justify-center bg-cue-blue">
+      <div className="w-[70%] h-auto flex gap-20 items-center justify-center">
+        <div className="w-[400px]">
+          <img src="https://www.dhla.org/wp-content/uploads/2019/08/LOGO-uvonhumboldt.png" alt="Logo CUE" />
+        </div>
+        <div className="flex flex-col text-start">
+          <p className="text-white font-calibri font-bold text-[50px]">ProjectCode</p>
+          <p className="text-white font-calibri text-[35px]">Un juego del Programa de Ingeniería de Software Dual</p>
+        </div>
+      </div>
+      <NavLink to='/soft-maze'>
+        <Button className="w-[250px] h-[70px] text-white font-calibri text-[30px]" color="secondary">Iniciar Juego</Button>
+      </NavLink>
+    </div>
   )
 }
