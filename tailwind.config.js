7678c6729d1d7b4c1e3eec6e2f6419c3580cb815
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-22 00:15:44 -0500

    Feat: Added DevTeamRoles background img

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'dev-team-intro': "linear-gradient(to right, rgba(19, 71, 117, .8), rgba(45, 116, 179, .8)), url('https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')"
+        'dev-team-intro': "linear-gradient(to right, rgba(19, 71, 117, .8), rgba(45, 116, 179, .8)), url('https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
+        'dev-team-roles': "linear-gradient(to right, rgba(57, 23, 79, .8), rgba(79, 1, 1, .8)), url('https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1206&q=80')",
      },
     },
   },
