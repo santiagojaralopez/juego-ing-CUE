4897bd2e3608f1f24296846b84fd9428ef0083a2
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-26 01:02:32 -0500

    Fix: Changed button label

--- src/Pages/DevTeamRoles.jsx
+++ src/Pages/DevTeamRoles.jsx
@@ -9,9 +9,14 @@ const DevTeamRoles = () => {
     <div className='h-screen flex justify-center items-center bg-dev-team-roles bg-cover'>
       <div class="w-4/5 h-4/5 p-10 flex flex-col gap-16 justify-center items-center rounded-2xl backdrop-blur-xl bg-white/70">
         <h1 className='font-cocogoose text-4xl font-bold'>Roles en el Equipo</h1>
-        <p className='font-cocogoose text-xl'>
-          Aquí podrás conocer en detalle los roles clave que puedes elegir para conformar tu equipo de desarrollo. Cada rol desempeña un papel fundamental en la creación del software exitoso. Tómate un momento para explorar y aprender más sobre las habilidades y responsabilidades de cada rol.
-        </p>
+        <div className="flex flex-col gap-2">
+          <p className='font-cocogoose text-xl'>
+            Aquí podrás conocer en detalle los roles clave que puedes elegir para conformar tu equipo de desarrollo. Cada rol desempeña un papel fundamental en la creación del software exitoso. Tómate un momento para explorar y aprender más sobre las habilidades y responsabilidades de cada rol.
+          </p>
+          <p className='font-cocogoose text-xl'>
+            Haz clic en cada nombre y lee un poco sobre sus funciones (los salarios mostrados en cada rol están basados en el mercado estadounidense).
+          </p>
+        </div>
         <div className='grid grid-cols-4 grid-flow-row gap-5'>
           {
             roles.map((role, index) => (
@@ -19,8 +24,8 @@ const DevTeamRoles = () => {
             ))
           }
         </div>
-        <NavLink to='/team-roles-info'>
-          <Button className="w-[250px] h-[70px] text-white font-calibri text-[30px]" color="success">Iniciar Juego</Button>
+        <NavLink to='/client-requirements'>
+          <Button className="w-[250px] h-[70px] text-white font-calibri text-[30px]" color="success">Siguiente</Button>
         </NavLink>
       </div>
     </div>
