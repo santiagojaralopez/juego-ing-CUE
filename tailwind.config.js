6501231a799a757875e811e489c0fd63e55d1354
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-26 01:00:12 -0500

    Feat: Added client-reqs component background

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
       backgroundImage: {
         'dev-team-intro': "linear-gradient(to right, rgba(19, 71, 117, .8), rgba(45, 116, 179, .8)), url('https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
         'dev-team-roles': "linear-gradient(to right, rgba(57, 23, 79, .8), rgba(79, 1, 1, .8)), url('https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1206&q=80')",
+        'client-reqs': "linear-gradient(to right, rgba(8, 99, 2, .8), rgba(1, 125, 92, .8)), url('https://images.unsplash.com/photo-1526738549149-8e07eca6c147?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80')",
      },
     },
   },
