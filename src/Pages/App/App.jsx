c8a8081ea9e7ff87a32c0caae2d7bf9f7f7b89bf
Santiago Jaramillo Lopez <santiagojaralopez@gmail.com>
2023-08-26 10:51:51 -0500

    Feat: Added RolesSelection component

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -6,6 +6,7 @@ import { SoftMaze } from '../SoftMaze';
 import DevTeamIntro from '../DevTeamIntro';
 import DevTeamRoles from '../DevTeamRoles';
 import ClientRequirements from '../ClientRequirements';
+import RolesSelection from '../RolesSelection';
 
 import './App.css';
 
@@ -16,7 +17,8 @@ const AppRoutes = () => {
     { path: '/soft-maze', element: <SoftMaze /> },
     { path: '/dev-team-intro', element: <DevTeamIntro /> },
     { path: '/team-roles-info', element: <DevTeamRoles /> },
-    { path: '/client-requirements', element: <ClientRequirements /> }
+    { path: '/client-requirements', element: <ClientRequirements /> },
+    { path: '/roles-selection', element: <RolesSelection />},
   ])
 
   return routes
